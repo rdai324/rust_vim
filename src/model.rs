5476eeb6d8b0bed39a83085d1a892895ec38b955
James He <james.ych.he@gmail.com>
2025-12-13 21:39:00 -0500

    trim dead code in model

--- src/model.rs
+++ src/model.rs
@@ -7,90 +7,22 @@ use std::io::BufReader;
 use std::io;
 use std::fs;
 
-#[derive(PartialEq, Eq, Debug)]
-pub enum Mode {
-    Normal,
-    Insert,
-    Command,
-    Search,
-    Visual,
-}
-
 #[derive(Debug)]
 pub struct EditorModel {
     pub rope: Rope,
-    pub cursor_x: usize, // column in chars
-    pub cursor_y: usize, // line index
-    pub mode: Mode,
     pub file_name: String,
-    pub command: String,
-    // pub search_query: String,
-
-    pub visual_start: Option<(usize, usize)>,
-
-    pub status: String,
-    pub search_matches: Vec<(usize, usize)>, // (line, col)
 }
 
 impl EditorModel {
     pub fn new(file_name: &str) -> Self {
         Self {
             rope: Rope::from_reader(BufReader::new(File::open(&file_name).unwrap())).unwrap(),
-            cursor_x: 0,
-            cursor_y: 0,
-            mode: Mode::Normal,
             file_name: String::from(file_name),
-            command: String::new(),
-            // search_query: String::new(),
-            visual_start: None,
-            status: "-- NORMAL --".into(),
-            search_matches: vec![],
         }
-    }
-
-    // Helpers to convert between (line, col) and char index
-    pub fn line_start_char(&self, line: usize) -> usize {
-        // line_to_char returns starting char index of the line
-        self.rope.line_to_char(line)
-    }
-
-    pub fn line_len_chars(&self, line: usize) -> usize {
-        let start = self.rope.line_to_char(line);
-        let next = if line + 1 < self.rope.len_lines() {
-            self.rope.line_to_char(line + 1)
-        } else {
-            self.rope.len_chars()
-        };
-        next - start
     }
 
     pub fn insert_char(&mut self, ch: char, char_idx: usize) {
         self.rope.insert_char(char_idx, ch);
-    }
-
-    pub fn backspace(&mut self) {
-        if self.cursor_x > 0 {
-            let char_idx = self.line_start_char(self.cursor_y) + self.cursor_x;
-            self.rope.remove((char_idx - 1)..char_idx);
-            self.cursor_x -= 1;
-        } else if self.cursor_y > 0 {
-            // join with previous line
-            let prev_len = self.line_len_chars(self.cursor_y - 1);
-            let char_idx = self.line_start_char(self.cursor_y);
-            // remove the newline char between lines (i.e. do nothing special; inserting/removing will handle)
-            // Actually rope represents lines without an explicit newline at the end, but line boundaries handled by indices
-            // To join, remove the line break by removing range char_idx..char_idx (no-op) but we need to move cursor
-            self.cursor_y -= 1;
-            self.cursor_x = prev_len;
-        }
-    }
-
-    pub fn newline(&mut self) {
-        let char_idx = self.line_start_char(self.cursor_y) + self.cursor_x;
-        self.rope.insert(char_idx, "
-");
-        self.cursor_y += 1;
-        self.cursor_x = 0;
     }
 
     pub fn delete_char(&mut self, char_idx: usize) {
@@ -99,43 +31,8 @@ impl EditorModel {
         }
     }
 
-    // Movement
-    pub fn move_left(&mut self) {
-        if self.cursor_x > 0 {
-            self.cursor_x -= 1;
-        } else if self.cursor_y > 0 {
-            self.cursor_y -= 1;
-            self.cursor_x = self.line_len_chars(self.cursor_y);
-        }
-    }
-
-    pub fn move_right(&mut self) {
-        let len = self.line_len_chars(self.cursor_y);
-        if self.cursor_x < len {
-            self.cursor_x += 1;
-        } else if self.cursor_y + 1 < self.rope.len_lines() {
-            self.cursor_y += 1;
-            self.cursor_x = 0;
-        }
-    }
-
-    pub fn move_up(&mut self) {
-        if self.cursor_y > 0 {
-            self.cursor_y -= 1;
-            self.cursor_x = cmp::min(self.cursor_x, self.line_len_chars(self.cursor_y));
-        }
-    }
-
-    pub fn move_down(&mut self) {
-        if self.cursor_y + 1 < self.rope.len_lines() {
-            self.cursor_y += 1;
-            self.cursor_x = cmp::min(self.cursor_x, self.line_len_chars(self.cursor_y));
-        }
-    }
-
     pub fn run_search(&mut self, search_query: &str) -> usize {
         let mut num_matches = 0;
-        self.search_matches.clear();
         if search_query.is_empty() {
             return 0;
         }
@@ -148,22 +45,6 @@ impl EditorModel {
             }
         }
         return num_matches;
-    }
-
-    pub fn jump_next_match(&mut self) {
-        if self.search_matches.is_empty() {
-            return;
-        }
-        for (y, x) in &self.search_matches {
-            if *y > self.cursor_y || (*y == self.cursor_y && *x > self.cursor_x) {
-                self.cursor_y = *y;
-                self.cursor_x = *x;
-                return;
-            }
-        }
-        let (y, x) = self.search_matches[0];
-        self.cursor_y = y;
-        self.cursor_x = x;
     }
 
     pub fn save(&self) -> io::Result<()> {
